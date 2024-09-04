fc44d66c3d184fd0ae5491e2a37b636a21b3b6fe
Ivan Rizkyanto <ivanzkyanto@outlook.com>
2024-09-04 20:44:24 +0700

    Move key location config to outside classpath and remove unnecessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
